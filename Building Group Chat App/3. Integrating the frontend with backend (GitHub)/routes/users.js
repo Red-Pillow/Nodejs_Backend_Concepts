e1e9ddbaf77eac8d2d9b47b85b8e3e26e27324f1
Chetan Sahu <sahu5chetan5@gmail.com>
2023-11-23 17:26:40 +0530

    Add files via upload

---
+++ Building Group Chat App/3. Integrating the frontend with backend (GitHub)/routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const userController = require('../controllers/users');
+
+
+
+const router = express.Router();
+
+router.post('/signup',userController.signup)
+
+
+
+
+module.exports = router;
